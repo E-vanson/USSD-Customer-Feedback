603da2bd0ad3b754caf86f27c626239a6811983a
Evans <denziedon@gmail.com>
2024-05-30 17:26:09 +0300

    Modified HIV

--- ussd/index.js
+++ ussd/index.js
@@ -211,7 +211,6 @@ app.get("/api/test", (req, res) => {
     defaultNext: "invalidOption",
   });
 
-<<<<<<< HEAD
   menu.state("Pay Contributions And Penalities", {
     run: function () {
       menu.con(
@@ -254,8 +253,6 @@ app.get("/api/test", (req, res) => {
     defaultNext: "invalidOption",
   });
 
-=======
->>>>>>> 1a9c4e1c04184c7db9c2afeafadd5d362f4105d5
   menu.state("National Aids Control Council", {
     run: function () {
       menu.con(
@@ -333,6 +330,27 @@ app.get("/api/test", (req, res) => {
   });
 
   menu.state("Blood Pressure", {
+    run: function () {
+      menu.con(
+        "Rate Service From 1-5:" +
+        "\n1. Very Poor" +
+        "\n2. Poor" +
+        "\n3. Good" +
+        "\n4. Very Good" +
+        "\n5. Excellent" 
+      );
+    },
+    next: {
+      1: "Very Poor",
+      2: "Poor",
+      3: "Good",
+      4: "Very Good",
+      5: "Excellent"
+    },
+    defaultNext: "invalidOption",
+  });
+
+  menu.state("HIV Awareness Education", {
     run: function () {
       menu.con(
         "Rate Service From 1-5:" +
